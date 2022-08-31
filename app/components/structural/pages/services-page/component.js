c75166f6833a7345f66f8c911bd67af054e452d4
Ryan Roderick <ryan@designmonster.com>
2022-08-31 18:10:54 -0400

    Added initial content and tweaked styling
    -Added some initial content
    -Fixed layouts for navigation for 'structural/routes' components
    -Renamed 'structural/routes' to 'structural/pages'

--- app/components/structural/pages/services-page/component.js
+++ app/components/structural/pages/services-page/component.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 
-export default class StructuralRoutesServicesComponent extends Component {
+export default class StructuralPagesServicesPageComponent extends Component {
   //routes for link-tos
   welcomeRoute = 'index';
   servicesRoute = 'services';
