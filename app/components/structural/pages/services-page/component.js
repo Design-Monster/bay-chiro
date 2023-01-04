adc2a97a0c2f8d9d1c8922ea75f118efb90283a0
Ryan Roderick <ryan@designmonster.com>
2023-01-04 17:32:39 -0500

    final coding commit before launch

--- app/components/structural/pages/services-page/component.js
+++ app/components/structural/pages/services-page/component.js
@@ -7,42 +7,25 @@ export default class StructuralPagesServicesPageComponent extends Component {
   visitRoute = 'visit';
   aboutRoute = 'about';
   //imageMasonryBlock images - only using on large and wide screens so no need for small and medium attributes
-  imageOneLarge =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/5c39e0c6-f684-4ba8-8b5a-0008d2189000/810x300';
-  imageOneLargeFallback =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/5c39e0c6-f684-4ba8-8b5a-0008d2189000/810x300';
-  imageOneWide =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/5c39e0c6-f684-4ba8-8b5a-0008d2189000/810x300';
-  imageOneWideFallback =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/5c39e0c6-f684-4ba8-8b5a-0008d2189000/810x300';
-  imageOneAltLabel =
-    'Doctor Curtis is performing an adjustment to a patient who is laying flat on a chiropractor table.';
-  imageTwoLarge =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/88268341-8a42-46bf-a396-643cf8b64c00/400x600';
-  imageTwoLargeFallback =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/88268341-8a42-46bf-a396-643cf8b64c00/400x600';
-  imageTwoWide =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/88268341-8a42-46bf-a396-643cf8b64c00/400x600';
-  imageTwoWideFallback =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/88268341-8a42-46bf-a396-643cf8b64c00/400x600';
+  imageOne =
+    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/791d5acd-13ba-4c83-a665-9de21dbeae00/850x250';
+  imageOneFallback =
+    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/791d5acd-13ba-4c83-a665-9de21dbeae00/850x250';
+  imageOneAltLabel = 'A Chiropractor table.';
+  imageTwo =
+    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/d829aa1a-99dc-4119-7045-9c604f2b5c00/400x600';
+  imageTwoFallback =
+    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/d829aa1a-99dc-4119-7045-9c604f2b5c00/400x600';
   imageTwoAltLabel = 'Front panel of ultrasound machine';
-  imageThreeLarge =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/dd5a2283-cca9-475f-a5df-14239aff7400/400x300';
-  imageThreeLargeFallback =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/dd5a2283-cca9-475f-a5df-14239aff7400/400x300';
-  imageThreeWide =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/dd5a2283-cca9-475f-a5df-14239aff7400/400x300';
-  imageThreeWideFallback =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/dd5a2283-cca9-475f-a5df-14239aff7400/400x300';
+  imageThree =
+    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/b57c6e86-1214-4d05-510c-40596a60b500/400x300';
+  imageThreeFallback =
+    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/b57c6e86-1214-4d05-510c-40596a60b500/400x300';
   imageThreeAltLabel = 'Doctor Curtis is taping the knee of a patient';
-  imageFourLarge =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/7cad6360-8008-436c-80ec-84bcdf210100/400x300';
-  imageFourLargeFallback =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/7cad6360-8008-436c-80ec-84bcdf210100/400x300';
-  imageFourWide =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/7cad6360-8008-436c-80ec-84bcdf210100/400x300';
-  imageFourWideFallback =
-    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/7cad6360-8008-436c-80ec-84bcdf210100/400x300';
+  imageFour =
+    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/2139456c-149b-4abe-5115-c1f647f9b000/400x300';
+  imageFourFallback =
+    'https://imagedelivery.net/uSmqQyv4nG5_KuZ4KHY0Ew/2139456c-149b-4abe-5115-c1f647f9b000/400x300';
   imageFourAltLabel =
-    'Doctor Curtis is performing a procedure to a patient who is laying flat on a chiropractor table.';
+    'Doctor Curtis is performing another procedure to a patient who is laying flat on a chiropractor table.';
 }
